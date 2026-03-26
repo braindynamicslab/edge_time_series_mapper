3018733b1128ebbd43ad0da493435228649dc9ca
csiu <alexcysiu@gmail.com>
2026-03-25 19:50:31 -0700

    updated io functions

--- fcn/io/fcn_io_check_fmri_data_availability.m
+++ fcn/io/fcn_io_check_fmri_data_availability.m
@@ -100,7 +100,7 @@ function fcn_io_check_fmri_data_availability()
             session = parts(2);
             
             % Look up batch (returns "" if not found)
-            batch = get_batch_for_scan(batch_table, subject, task, session);
+            batch = fcn_io_lookup_batch_table(batch_table, subject, task, session);
             if strlength(batch) == 0
                 % Batch not found - file doesn't exist
                 continue;
@@ -161,19 +161,3 @@ function combos = build_task_session_combinations(tasks, task_sessions, rest_ses
     combos = [other_combos; rest_combos];
 end
 
-function batch = get_batch_for_scan(batch_table, subject, task, session)
-    % Look up batch identifier for a specific scan
-    
-    % Vectorized row matching
-    is_match = (batch_table.subject == subject) & ...
-               strcmp(batch_table.task, task) & ...
-               strcmp(batch_table.session, session);
-    
-    match_idx = find(is_match, 1);  % Find first match
-    
-    if isempty(match_idx)
-        batch = "";
-    else
-        batch = batch_table.batch(match_idx);
-    end
-end
